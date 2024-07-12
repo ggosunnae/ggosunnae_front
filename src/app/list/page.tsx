6f6426700b3b5d6d46dabff070fed1a8b02d5b49
Hannah <hannah1155@naver.com>
2024-07-13 00:47:26 +0900

    💩 next link 설치 문제...

---
+++ src/app/list/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/Layout/Header";
+
+const page = () => {
+  return (
+    <>
+      <Header />
+      <main>{/* Main content of the page */}</main>
+    </>
+  );
+};
+
+export default page;
