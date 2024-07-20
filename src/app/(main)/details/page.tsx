f5ba9d5bdc8e481935eec8380898f48d61f26f0f
Hannah <hannah1155@naver.com>
2024-07-20 21:55:31 +0900

    :fire: details.tsx 내용 삭제

--- src/app/(main)/details/page.tsx
+++ src/app/(main)/details/page.tsx
@@ -1,14 +1,1 @@
-import Comment from "@/components/Common/Comment";
-import React from "react";
 
-export default function page() {
-  return (
-    <>
-      <div className="bg-white">
-        <main className="no-scrollbar h-[calc(100vh-100px)]">
-          <Comment />
-        </main>
-      </div>
-    </>
-  );
-}
