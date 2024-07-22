bd2a8806c6284f657c1811b5320b9e47bd987068
Kim Ji Yu <spbabo97@naver.com>
2024-07-22 22:09:08 +0900

    :bug: 없어진 Chip 복구

--- src/components/common/Chip.tsx
+++ src/components/common/Chip.tsx
@@ -2,22 +2,9 @@
 import { MouseEventHandler } from "react";
 
 interface ChipT {
-  /**
-   * 타입은 마우스 커서를 의미 합니다. button을 경우 point가 되고 default일 기본 커서 입니다.
-   * 기본값은 default 입니다.
-   */
   type?: "button" | "default";
-  /**
-   * 버튼이 활성화 되어있는것을 지정합니다. label가 동일하면 활성화 상태가 됩니다.
-   */
   active?: string;
-  /**
-   * 버튼에 들어갈 텍스트를 입력합니다.
-   */
   label?: string;
-  /**
-   * onClick 함수를 넣을수 있습니다.
-   */
   onClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
