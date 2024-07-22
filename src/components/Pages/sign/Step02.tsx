b0fb6627022ac0238bdb34f9ddbec6760678bc6c
Kim Ji Yu <spbabo97@naver.com>
2024-07-22 10:13:27 +0900

    :sparkles:  파일 업로드기능 구현

--- src/components/Pages/sign/Step02.tsx
+++ src/components/Pages/sign/Step02.tsx
@@ -1,17 +1,38 @@
-import { useRef } from "react";
+import { ChangeEvent, useRef, useState } from "react";
 import UploadIcon from "@/asset/icons/Upload.svg";
 import Input from "@/components/Common/Input/Input";
+import { FieldValues, useForm } from "react-hook-form";
+import Image from "next/image";
 
 interface StepType {
   type: string;
 }
 
 const Step02 = ({ type }: StepType) => {
+  const [userImage, setUserImage] = useState("");
   const fileRef = useRef<HTMLInputElement>(null);
+
+  const { register, handleSubmit, watch } = useForm();
+  const name = watch("name");
 
   const onClickHandler = () => {
     if (!fileRef.current) return;
     fileRef.current.click();
+  };
+
+  const onUploadImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (!file) return;
+
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+      setUserImage(fileReader.result as string);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
+  const onSubmit = (event: FieldValues) => {
+    console.log(event);
   };
 
   return (
@@ -19,21 +40,46 @@ const Step02 = ({ type }: StepType) => {
       <h4 className="mt-6 text-center text-2xl font-semibold leading-9 tracking-tight">
         {type === "daengju" ? "댕주" : "댕친"}로 가입하기
       </h4>
-      <div
-        onClick={onClickHandler}
-        className="mx-auto mt-4 flex size-[164px] cursor-pointer flex-col items-center justify-center rounded-full bg-[#D9D9D9] leading-6 tracking-tight text-grayscale-gray2"
-      >
-        사진 추가하기
-        <UploadIcon className="mt-[5px]" />
-        <input ref={fileRef} type="file" className="hidden" />
-      </div>
 
-      <div className="mt-[41px]">
-        <label htmlFor="" className="font-semibold leading-6 tracking-tight">
-          닉네임*
-        </label>
-        <Input approve={true} />
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div
+          onClick={onClickHandler}
+          className={`relative mx-auto mt-4 flex size-[164px] cursor-pointer flex-col items-center justify-center overflow-hidden rounded-full ${!userImage ? "bg-[#D9D9D9]" : "border"} leading-6 tracking-tight text-grayscale-gray2`}
+        >
+          {!userImage ? (
+            <>
+              사진 추가하기
+              <UploadIcon className="mt-[5px]" />
+            </>
+          ) : (
+            <Image
+              src={userImage}
+              fill
+              alt="유저 이미지"
+              className="object-cover"
+            />
+          )}
+          <input
+            ref={fileRef}
+            type="file"
+            accept="image/*"
+            className="hidden"
+            onChange={onUploadImageChange}
+          />
+        </div>
+
+        <div className="mt-[41px]">
+          <label htmlFor="" className="font-semibold leading-6 tracking-tight">
+            닉네임*
+          </label>
+          <Input
+            register={register("name")}
+            approve={true}
+            placeholder="닉네임을 입력해주세요"
+            type="text"
+          />
+        </div>
+      </form>
     </section>
   );
 };
