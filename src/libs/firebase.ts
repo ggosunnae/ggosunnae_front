53a900b144cb43ef7beb5232b243973fd5a395fd
Kim Ji Yu <spbabo97@naver.com>
2024-07-23 19:31:40 +0900

    :sparkles: FIREBASE 설치

---
+++ src/libs/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB53paZhjPdM0q4nhd9YZyVZuZCrwd_fM8",
+  authDomain: "ggosunnae-6eb10.firebaseapp.com",
+  projectId: "ggosunnae-6eb10",
+  storageBucket: "ggosunnae-6eb10.appspot.com",
+  messagingSenderId: "545280276408",
+  appId: "1:545280276408:web:1dcb124116690af803c14d",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
